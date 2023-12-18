4fff9629d62e258e5ac977ec3390e0ee094b1b14
Keshwam <pandeykeshwam1122@gmail.com>
2023-12-18 11:42:47 +0530

    Code Progress

--- yes.py
+++ yes.py
@@ -3,4 +3,7 @@ pygame.init()
 gamewindow=pygame.display.set_mode((1200,500)) #display size
 pygame.display.set_caption("YESSSSSS") #game ka naam
 
-exit_game= "";
+# game specific variable 
+exit_game= False
+game_over= False
+
